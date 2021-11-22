d45649aa3e3a8dbc347929dfd465026332e45ca2
byynaryy <endeki@gmail.com>
2021-11-23 00:17:21 +0100

    feat: dislay a menu object as a json object

--- src/main/java/com/alacarte/restaurantmenu/RestaurantmenuApplication.java
+++ src/main/java/com/alacarte/restaurantmenu/RestaurantmenuApplication.java
@@ -1,11 +1,14 @@
 package com.alacarte.restaurantmenu;
 
+import com.alacarte.restaurantmenu.menu.Menu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 @SpringBootApplication
@@ -17,8 +20,40 @@ public class RestaurantmenuApplication {
 	}
 
 	@GetMapping
-	public List<String> helloWorld() {
-		return List.of("Hello",  "world");
+	public List<Menu> helloWorld() {
+		ArrayList<String> dietaryOptions  = new ArrayList<>();
+		dietaryOptions.add("Locally Produced");
+		dietaryOptions.add("Vegetarian dishes");
+
+		HashMap<String, Float> starter = new HashMap<>();
+		starter.put("Asperges blanches et burrata", 27.99f);
+		starter.put("sauce sakura", 19.99f);
+
+		HashMap<String, Float> mainCourse = new HashMap<>();
+        mainCourse.put("Saumon mi-cuit chinuki mariné au miso", 45.50f);
+		mainCourse.put("Poulpe poêlé, purée de piquillos", 35.50f);
+
+		HashMap<String, Float> dessert = new HashMap<>();
+		dessert.put("Assiette de fruits de saison", 13.00f);
+		dessert.put("Nougat glacé au matcha,", 15.00F);
+
+		HashMap<String, Float> drinks = new HashMap<>();
+		drinks.put("Coffee", 2.5f);
+		drinks.put("Bottle of wine", 29.00f);
+
+
+		return List.of(
+				new Menu(
+						1L,
+						"Mama Moto",
+						45.45f,
+						dietaryOptions,
+						starter,
+						mainCourse,
+						dessert,
+						drinks
+				)
+		);
 	}
 
 }
