11a9c8a1424d9566eb591daf76e3ad61f65f0797
byynaryy <endeki@gmail.com>
2021-11-27 23:15:44 +0100

    feat: inject the menu service dependecy into the menu controller using the @Service and @Autowired annotations

--- src/main/java/com/alacarte/restaurantmenu/menu/MenuController.java
+++ src/main/java/com/alacarte/restaurantmenu/menu/MenuController.java
@@ -1,5 +1,6 @@
 package com.alacarte.restaurantmenu.menu;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,40 +13,16 @@ import java.util.List;
 @RequestMapping(path = "api/menu")
 public class MenuController {
 
+    private final MenuService menuService;
+
+    @Autowired
+    public MenuController(MenuService menuService) {
+        this.menuService = menuService;
+    }
+
     @GetMapping
     public List<Menu> getMenus() {
-        ArrayList<String> dietaryOptions  = new ArrayList<>();
-        dietaryOptions.add("Locally Produced");
-        dietaryOptions.add("Vegetarian dishes");
+        return menuService.getMenus();
 
-        HashMap<String, Float> starter = new HashMap<>();
-        starter.put("Asperges blanches et burrata", 27.99f);
-        starter.put("sauce sakura", 19.99f);
-
-        HashMap<String, Float> mainCourse = new HashMap<>();
-        mainCourse.put("Saumon mi-cuit chinuki mariné au miso", 45.50f);
-        mainCourse.put("Poulpe poêlé, purée de piquillos", 35.50f);
-
-        HashMap<String, Float> dessert = new HashMap<>();
-        dessert.put("Assiette de fruits de saison", 13.00f);
-        dessert.put("Nougat glacé au matcha,", 15.00F);
-
-        HashMap<String, Float> drinks = new HashMap<>();
-        drinks.put("Coffee", 2.5f);
-        drinks.put("Bottle of wine", 29.00f);
-
-
-        return List.of(
-                new Menu(
-                        1L,
-                        "Mama Moto",
-                        45.45f,
-                        dietaryOptions,
-                        starter,
-                        mainCourse,
-                        dessert,
-                        drinks
-                )
-        );
     }
 }
--- src/main/java/com/alacarte/restaurantmenu/menu/MenuService.java
+++ src/main/java/com/alacarte/restaurantmenu/menu/MenuService.java
@@ -1,4 +1,47 @@
 package com.alacarte.restaurantmenu.menu;
 
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+@Service
 public class MenuService {
+
+    public List<Menu> getMenus() {
+        ArrayList<String> dietaryOptions  = new ArrayList<>();
+        dietaryOptions.add("Locally Produced");
+        dietaryOptions.add("Vegetarian dishes");
+
+        HashMap<String, Float> starter = new HashMap<>();
+        starter.put("Asperges blanches et burrata", 27.99f);
+        starter.put("sauce sakura", 19.99f);
+
+        HashMap<String, Float> mainCourse = new HashMap<>();
+        mainCourse.put("Saumon mi-cuit chinuki mariné au miso", 45.50f);
+        mainCourse.put("Poulpe poêlé, purée de piquillos", 35.50f);
+
+        HashMap<String, Float> dessert = new HashMap<>();
+        dessert.put("Assiette de fruits de saison", 13.00f);
+        dessert.put("Nougat glacé au matcha,", 15.00F);
+
+        HashMap<String, Float> drinks = new HashMap<>();
+        drinks.put("Coffee", 2.5f);
+        drinks.put("Bottle of wine", 29.00f);
+
+
+        return List.of(
+                new Menu(
+                        1L,
+                        "Mama Moto",
+                        45.45f,
+                        dietaryOptions,
+                        starter,
+                        mainCourse,
+                        dessert,
+                        drinks
+                )
+        );
+    }
 }
