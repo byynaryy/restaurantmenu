fd76e7a5b9031e912e8958d4caa4669a6718e62a
byynaryy <endeki@gmail.com>
2021-12-15 22:53:27 +0100

    feat: use menu repository to get/find all the posts

--- src/main/java/com/alacarte/restaurantmenu/menu/MenuService.java
+++ src/main/java/com/alacarte/restaurantmenu/menu/MenuService.java
@@ -18,39 +18,7 @@ public class MenuService {
     }
 
     public List<Menu> getMenus() {
-        ArrayList<String> dietaryOptions  = new ArrayList<>();
-        dietaryOptions.add("Locally Produced");
-        dietaryOptions.add("Vegetarian dishes");
-
-        HashMap<String, Float> starter = new HashMap<>();
-        starter.put("Asperges blanches et burrata", 27.99f);
-        starter.put("sauce sakura", 19.99f);
-
-        HashMap<String, Float> mainCourse = new HashMap<>();
-        mainCourse.put("Saumon mi-cuit chinuki mariné au miso", 45.50f);
-        mainCourse.put("Poulpe poêlé, purée de piquillos", 35.50f);
-
-        HashMap<String, Float> dessert = new HashMap<>();
-        dessert.put("Assiette de fruits de saison", 13.00f);
-        dessert.put("Nougat glacé au matcha,", 15.00F);
-
-        HashMap<String, Float> drinks = new HashMap<>();
-        drinks.put("Coffee", 2.5f);
-        drinks.put("Bottle of wine", 29.00f);
-
-
-        return List.of(
-                new Menu(
-                        1L,
-                        "Mama Moto",
-                        45.45f,
-                        dietaryOptions,
-                        starter,
-                        mainCourse,
-                        dessert,
-                        drinks
-                )
-        );
+        return menuRepository.findAll();
     }
 
     public void addNewMenu(Menu menu) {
